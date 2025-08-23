122dc56e5cf2c5f20c0fbee3013c5f680bba37eb
Jakob Holterhoff <jholterh@student.42berlin.de>
2025-08-23 20:22:48 +0200

    feat: wrong files added to see the difference

--- 4ex00/main.cpp
+++ 4ex00/main.cpp
@@ -1,20 +1,45 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+#include "WrongDog.hpp"
+
 
 int main()
 {
+    // Correct hierarchy (with virtual)
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
+
+    i->makeSound(); // will output the cat sound!
+    j->makeSound(); // will output the dog sound!
+    meta->makeSound(); // will output the animal sound!
+
     delete meta;
     delete j;
     delete i;
+
+    std::cout << "---- WrongAnimal tests ----" << std::endl;
+
+    // Wrong hierarchy (no virtual)
+    const WrongAnimal* wrongMeta = new WrongAnimal();
+    const WrongAnimal* wrongCat = new WrongCat();
+    const WrongAnimal* wrongDog = new WrongDog();
+
+    std::cout << wrongCat->getType() << " " << std::endl;
+    wrongCat->makeSound(); // will output WrongAnimal's sound, not WrongCat's!
+
+    std::cout << wrongDog->getType() << " " << std::endl;
+    wrongDog->makeSound(); // will output WrongAnimal's sound, not WrongDog's!
+
+    delete wrongMeta;
+    delete wrongCat;
+    delete wrongDog;
 
     return 0;
 }
