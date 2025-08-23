aacd23797aaf42782d43610802b794e235bb801c
Jakob Holterhoff <jholterh@student.42berlin.de>
2025-08-23 20:11:53 +0200

    feat: dog, animal and cat for polymorphism added

---
+++ 4ex00/Cat.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+#include <string>
+#include <iostream>
+
+class Cat : public Animal
+{
+    public:
+        Cat();
+        Cat(const Cat &copy);
+        Cat &operator=(const Cat &other);
+        ~Cat();
+        void makeSound() const;
+
+};
+
+#endif
---
+++ 4ex00/Dog.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+#include <string>
+#include <iostream>
+
+class Dog : public Animal
+{
+    public:
+        Dog();
+        Dog(const Dog &copy);
+        Dog &operator=(const Dog &other);
+        ~Dog();
+        void makeSound() const;
+};
+
+#endif
