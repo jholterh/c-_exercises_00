ff6a74a891aa4e116f8ee074e96652cc474e2caf
Jakob Holterhoff <jholterh@student.42berlin.de>
2025-08-25 10:31:08 +0200

    feat: built function AMateria.hpp

---
+++ 4ex03/AMateria.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <string>
+#include "ICharacter.hpp"
+
+class AMateria
+{
+    public:
+        AMateria();
+        AMateria(const AMateria& copy);
+        AMateria& operator=(const AMateria& other);
+        virtual ~AMateria();
+        AMateria(const std::string& type);
+        const std::string& getType()const;
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+
+    protected:
+        std::string type;
+};
+
+#endif
