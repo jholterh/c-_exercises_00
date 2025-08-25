d60ff576b5fdd13dd116283a3e63ae17e67ad183
Jakob Holterhoff <jholterh@student.42berlin.de>
2025-08-25 12:30:25 +0200

    feat: added a test make file

---
+++ 4ex03/main.cpp
@@ -0,0 +1,32 @@
+#include "IMateriaSource.hpp"
+#include "MateriaSource.hpp"
+#include "Ice.hpp"
+#include "Cure.hpp"
+#include "ICharacter.hpp"
+#include "Character.hpp"
+
+int main()
+{
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+
+    ICharacter* me = new Character("me");
+
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+
+    ICharacter* bob = new Character("bob");
+
+    me->use(0, *bob); // Should print: * shoots an ice bolt at bob *
+    me->use(1, *bob); // Should print: * heals bob's wounds *
+
+    delete bob;
+    delete me;
+    delete src;
+
+    return 0;
+}
