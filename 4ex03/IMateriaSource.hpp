933088cccda30ec9132cb3261e0fc1214f21bf31
Jakob Holterhoff <jholterh@student.42berlin.de>
2025-08-25 11:57:46 +0200

    feat: added interface for MateriaSource

---
+++ 4ex03/IMateriaSource.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef IMATERIASOURCE_HPP
+#define IMATERIASOURCE_HPP
+
+#include <string>
+#include "AMateria.hpp"
+
+class IMateriaSource
+{
+    public:
+        virtual ~IMateriaSource() {}
+        virtual void learnMateria(AMateria* m) = 0;
+        virtual AMateria* createMateria(const std::string& type) = 0;
+};
+
+#endif
